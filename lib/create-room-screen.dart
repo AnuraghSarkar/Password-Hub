13acd5354e9826ac994043867224fad548fab894
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-02 21:58:58 +0545

    Create Room Page Finished

--- lib/create-room-screen.dart
+++ lib/create-room-screen.dart
@@ -11,6 +11,8 @@ class CreateRoomScreen extends StatefulWidget {
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _roomNameController = TextEditingController();
+  late String? _maxRoundsValue;
+  late String? _maxRoomSizeValue;
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +40,73 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
             child: CustomTextField(
               controller: _roomNameController,
               hintText: 'Enter Your Room Name',
+            ),
+          ),
+          SizedBox(height: 20),
+          DropdownButton(
+            focusColor: Color(0xffdcdce8),
+            items: ['2', '5', '10', '15']
+                .map((e) => DropdownMenuItem(
+                    child: Text(
+                      e,
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    value: e))
+                .toList(),
+            hint: Text(
+              'Select Number of Rounds',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500),
+            ),
+            onChanged: (String? e) {
+              setState(() {
+                _maxRoundsValue = e;
+              });
+            },
+          ),
+          SizedBox(height: 20),
+          DropdownButton(
+            focusColor: Color(0xffdcdce8),
+            items: ['2', '3', '4', '5', '6', '7', '8']
+                .map((e) => DropdownMenuItem(
+                    child: Text(
+                      e,
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    value: e))
+                .toList(),
+            hint: Text(
+              'Select Room Size',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500),
+            ),
+            onChanged: (String? e) {
+              setState(() {
+                _maxRoomSizeValue = e;
+              });
+            },
+          ),
+          SizedBox(height: 40),
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.orange),
+                textStyle: MaterialStateProperty.all<TextStyle>(
+                    TextStyle(color: Colors.white)),
+                minimumSize: MaterialStateProperty.all<Size>(Size(
+                    MediaQuery.of(context).size.width / 2.5,
+                    MediaQuery.of(context).size.height * .06))),
+            child: const Text(
+              'Create Room',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black),
             ),
           ),
         ],
--- lib/widgets/customtextfield.dart
+++ lib/widgets/customtextfield.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
-  CustomTextField({Key? key, required this.controller, required this.hintText})
+  const CustomTextField({Key? key, required this.controller, required this.hintText})
       : super(key: key);
 
   @override
