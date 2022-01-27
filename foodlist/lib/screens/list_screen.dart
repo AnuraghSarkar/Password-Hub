b7f54d46178f238d38f4e62b57720e7ca10d59b3
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-27 20:47:21 +0545

    displayed mock data

--- foodlist/lib/screens/list_screen.dart
+++ foodlist/lib/screens/list_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:foodlist/mocks/grocery_items_mock.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final items = groceryItems;
     return Scaffold(
       appBar: AppBar(title: const Text('Foodie List')),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return Card(
+            child: Text(item['name']),
+          );
+        },
       ),
     );
   }
