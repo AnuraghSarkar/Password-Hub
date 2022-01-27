ccbd569f593306f611f364316605ca25913adc34
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-27 20:42:07 +0545

    created mock grocery file for testing

--- foodlist/lib/main.dart
+++ foodlist/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:foodlist/screens/ListScreen.dart';
+import 'package:foodlist/screens/list_screen.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ foodlist/lib/mocks/grocery_items_mock.dart
@@ -0,0 +1,14 @@
+const List<Map<String, dynamic>> groceryItems = [
+  {
+    'id': 1,
+    'name': 'Coffee',
+    'category': 'dairy',
+    'purchased': false,
+  },
+  {
+    'id': 2,
+    'name': 'goat',
+    'category': 'meat',
+    'purchased': false,
+  }
+];
