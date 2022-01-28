5ff2d26ce6dde41d902d640b6eb0e16f79807379
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-28 20:16:24 +0545

    :fire: added biometrics and pattern

--- foodlist/lib/screens/auth_screen.dart
+++ foodlist/lib/screens/auth_screen.dart
@@ -1,83 +1,56 @@
 import 'package:foodlist/screens/list_screen.dart';
 import 'package:foodlist/services/service_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 
 class BiometericScreen extends StatelessWidget {
   const BiometericScreen({Key? key}) : super(key: key);
 
-  static const colorizeColors = [
-    Colors.purple,
-    Colors.blue,
-    Colors.yellow,
-    Colors.red,
-  ];
-
-  static const colorizeTextStyle = TextStyle(
-    fontSize: 50.0,
-    fontFamily: 'Horizon',
-  );
-
   @override
   Widget build(BuildContext context) {
-    // return SafeArea(
-    //     child: Scaffold(
-    //   body: Center(
-    //       child: Column(
-    //     mainAxisAlignment: MainAxisAlignment.center,
-    //     crossAxisAlignment: CrossAxisAlignment.center,
-    //     children: [
-    //       IconButton(
-    //         icon: const Icon(Icons.fingerprint_outlined),
-    //         iconSize: 100,
-    //         onPressed: () async {
-    //           final isAuthenticate = await LocalAuthApi.authenticate();
-    //           if (isAuthenticate == true) {
-    //             Navigator.pushReplacement(
-    //                 context,
-    //                 MaterialPageRoute(
-    //                   builder: (context) => const ListScreen(),
-    //                 ));
-    //           } else {
-    //             return;
-    //           }
-    //         },
-    //       )
-    //     ],
-    //   )),
-    // ));
-    return SizedBox(
-      width: 250.0,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          AnimatedTextKit(
-            animatedTexts: [
-              ColorizeAnimatedText(
-                'Larry Page',
-                textStyle: colorizeTextStyle,
-                colors: colorizeColors,
+          const Center(
+            child: Text(
+              'Authenticate For Foodie List',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: 60.0,
+                height: 1.6,
+                shadows: [
+                  Shadow(
+                    blurRadius: 12.0,
+                    color: Colors.blue,
+                    offset: Offset(6.0, 6.0),
+                  ),
+                ],
               ),
-              ColorizeAnimatedText(
-                'Bill Gates',
-                textStyle: colorizeTextStyle,
-                colors: colorizeColors,
-              ),
-              ColorizeAnimatedText(
-                'Steve Jobs',
-                textStyle: colorizeTextStyle,
-                colors: colorizeColors,
-              ),
-            ],
-            isRepeatingAnimation: true,
-            onTap: () {
-              print("Tap Event");
-            },
+            ),
           ),
+          IconButton(
+            icon: const Icon(Icons.fingerprint_outlined),
+            iconSize: 100,
+            onPressed: () async {
+              final isAuthenticate = await LocalAuthApi.authenticate();
+              if (isAuthenticate == true) {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ListScreen(),
+                    ));
+              } else {
+                return;
+              }
+            },
+          )
         ],
-      ),
-    );
+      )),
+    ));
   }
 
   Widget buildT(String text, bool checked) {
