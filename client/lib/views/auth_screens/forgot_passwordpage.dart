f4e0fd9202b2c8aad76b86ebe5cdc5532b25c13b
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-24 22:05:07 +0545

    :hammer: screen for login page

---
+++ client/lib/views/auth_screens/forgot_passwordpage.dart
@@ -0,0 +1,152 @@
+//For reset the main password
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:client/controller/auth_controller/verify_and_forgot_master_pass_controller.dart';
+import 'package:client/services/auth_services/login_services.dart';
+import 'package:client/views/error_screens/server_down.dart';
+import 'package:client/widgets/snack_bar.dart';
+
+import '../../constants.dart';
+
+class ForgotPassword extends StatelessWidget {
+  ForgotPassword({Key? key}) : super(key: key);
+  final _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    var forgotPasswordController = Get.put(VerifyAndForgotMasterPass());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Forgot Password"),
+      ),
+      body: Obx(
+        () => SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15),
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                children: [
+                  SizedBox(height: 20.0),
+                  Text(
+                    "Reset password",
+                    style: GoogleFonts.poppins(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Enter the email associated with your account and we'll send  an email with instructions to reset your password",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF556274),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Text(
+                    "Email address",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF556274),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (!value!.contains(
+                          RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"))) {
+                        return "Enter Proper Email";
+                      }
+                    },
+                    decoration: InputDecoration(
+                        labelText: "Email",
+                        hintText: "johndoe@gmail.com",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        focusColor: Colors.white,
+                        prefixIcon: Icon(CupertinoIcons.mail)),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      var mail = _emailController.text;
+                      if (forgotPasswordController.isButtonEnabled.value) {
+                        if (_formKey.currentState!.validate()) {
+                          submitMail(mail, forgotPasswordController, context);
+                        }
+                      }
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: kSecondaryColor,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "${forgotPasswordController.forgotButtonText.value}",
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      height: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+submitMail(email, controller, context) async {
+  controller.setForgotButtonText("Verifying...");
+  controller.toggleButton(false);
+  var response = await LoginServices().resetPassword(email);
+
+  if (response.statusCode == 400) {
+    errorSnack("${response.body["message"]}", context);
+    controller.toggleButton(true);
+    controller.setForgotButtonText("Send Instructions");
+  } else if (response.statusCode == 200) {
+    successSnack("Link sent to registered email id", context);
+    controller.setForgotButtonText("Success");
+    controller.toggleButton(true);
+    Get.toNamed('/mail-sent', arguments: {
+      "type": "password recover",
+      "button": "try again with another mail",
+      "nextRoute": "/login",
+      "sentRoute": "/forgot-pass"
+    });
+  } else if (response.statusCode == 500) {
+    errorSnack("Internal Server Error", context);
+    controller.setForgotButtonText("Send Instructions");
+    controller.toggleButton(true);
+  } else {
+    Get.off(ServerDown());
+  }
+}
---
+++ client/lib/views/auth_screens/loginpage.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:client/constants.dart';
+import 'package:client/controller/auth_controller/login_controller.dart';
+import 'package:client/main.dart';
+import 'package:client/services/auth_services/login_services.dart';
+import 'package:client/views/error_screens/server_down.dart';
+import 'package:client/widgets/snack_bar.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+  //email controller get email of user
+  final _emailController = TextEditingController();
+  //password controller to get password from user
+  final _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final loginController = Get.put(LoginController());
+    return WillPopScope(
+      onWillPop: () async {
+        Get.offNamed("/welcome");
+        return false;
+      },
+      child: Obx(
+        () => Scaffold(
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Form(
+                key: _formKey,
+                child: ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 18.0),
+                      child: Image.asset(
+                        "assets/logo/logo.png",
+                        height: 80,
+                        width: 80.0,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Center(
+                        child: Text(
+                          "Password Hub",
+                          style: GoogleFonts.poppins(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w600,
+                            color: kMainColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Center(
+                      child: Text(
+                        "Sign In with your email and password",
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (!value!.contains(
+                            RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"))) {
+                          return "Invalid Email";
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          hintText: "johndoe@gmail.com",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                          ),
+                          focusColor: Colors.white,
+                          prefixIcon: Icon(CupertinoIcons.mail)),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: (loginController.isPasswordVisibile.value)
+                          ? false
+                          : true,
+                      keyboardType: TextInputType.visiblePassword,
+                      validator: (value) {
+                        if (value!.length < 6) {
+                          return "Enter proper password";
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "*************",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        focusColor: Colors.white,
+                        prefixIcon: Icon(CupertinoIcons.lock),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            loginController.togglePassword();
+                          },
+                          icon: Icon((loginController.isPasswordVisibile.value)
+                              ? CupertinoIcons.eye_slash
+                              : CupertinoIcons.eye),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        var email = _emailController.text;
+                        var password = _passwordController.text;
+                        if (loginController.isButtonEnabled.value) {
+                          if (_formKey.currentState!.validate()) {
+                            _submitDetails(
+                                email, password, loginController, context);
+                          }
+                        }
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: kMainColor,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "${loginController.buttonText.value}",
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        height: 50.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Get.toNamed('/forgot-pass');
+                      },
+                      child: Text(
+                        "Forgot Password",
+                        style: GoogleFonts.poppins(
+                            color: Colors.grey,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.underline),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Dont't have an account?",
+                          style: GoogleFonts.poppins(),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Get.toNamed('/register');
+                          },
+                          child: Text(
+                            "Sign Up",
+                            style: GoogleFonts.poppins(
+                              color: kMainColor,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+_submitDetails(email, password, controller, context) async {
+  controller.toggleButton(false);
+  controller.setButtonText("Verifying...");
+  var response = await LoginServices().loginUser(email, password);
+  if (response.body["message"] == "User is not verified") {
+    controller.setButtonText("Sign In");
+    controller.toggleButton(true);
+    errorSnack("You are not verified !", context);
+    Get.offAllNamed(
+      '/mail-sent',
+      arguments: {
+        "type": "mail confirmation",
+        "button": "Try again with proper mail",
+        "nextRoute": "/master",
+        "sentRoute": "/register"
+      },
+    );
+  } else if (response.body["message"] == "Master Password is not created.") {
+    controller.setButtonText("Sign In");
+    controller.toggleButton(true);
+    errorSnack("Master Password is not created.", context);
+    Get.offAllNamed("/master");
+  } else if (response.statusCode == 400) {
+    errorSnack("${response.body["message"]}", context);
+    controller.toggleButton(true);
+    controller.setButtonText("Sign In");
+  } else if (response.statusCode == 200) {
+    controller.setButtonText("Logged In");
+    var authToken = response.body["authToken"];
+    controller.toggleButton(true);
+    /* decode() method will decode your token's payload */
+    Map<String, dynamic> decodedToken = JwtDecoder.decode(authToken);
+    box.write("id", decodedToken["id"]);
+    box.write("auth-token", authToken);
+    Get.offAllNamed('/master-pass');
+  } else if (response.statusCode == 500) {
+    errorSnack("Internal Server Error", context);
+    controller.toggleButton(true);
+    controller.setButtonText("Sign In");
+  } else {
+    Get.off(ServerDown());
+  }
+}
