f945140b876a3eb9ec95defc408928b00bc678e2
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-24 22:22:52 +0545

    :clown_face: added theme service for passwordhub

---
+++ client/lib/services/theme_services.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:client/main.dart';
+
+class ThemeServices {
+  final storageKey = "isDarkMode";
+
+  ThemeMode getThemeMode() {
+    return isSavedDarkMode() ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  bool isSavedDarkMode() {
+    return box.read(storageKey) ?? false;
+  }
+
+  void saveThemeMode(bool isDarkMode) {
+    box.write(storageKey, isDarkMode);
+  }
+
+  void changeThemeMode() {
+    Get.changeThemeMode(isSavedDarkMode() ? ThemeMode.light : ThemeMode.dark);
+    saveThemeMode(!isSavedDarkMode());
+  }
+}
