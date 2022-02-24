5d6b6d4bc8974688d89393a3888adb5f6c222307
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-24 21:45:38 +0545

    :green_heart: added category for icon

---
+++ client/lib/data/category_icon.dart
@@ -0,0 +1,23 @@
+final Map categoryIcon = {
+  "social": "https://cdn-icons-png.flaticon.com/512/25/25360.png",
+  "finance":
+      "https://cdn4.iconfinder.com/data/icons/unigrid-finance/60/014_cash_coin_coins_money_finance_currency-512.png",
+  "entertainment":
+      "https://cdn1.iconfinder.com/data/icons/literary-genres-1/64/entertainment-film-video-footage-vlog-record-256.png",
+  "food_and_drink":
+      "https://cdn4.iconfinder.com/data/icons/food-and-beverages-4/44/food-11-256.png",
+  "education":
+      "https://cdn2.iconfinder.com/data/icons/school-supplies-7/64/164_school-open-study-education-book-library-256.png",
+  "dating":
+      "https://cdn0.iconfinder.com/data/icons/social-circle-3/72/Tinder-256.png",
+  "design":
+      "https://cdn2.iconfinder.com/data/icons/flat-seo-web-ikooni/128/flat_seo2-50-512.png",
+  "business":
+      "https://cdn2.iconfinder.com/data/icons/thin-business-1/24/thin-1089_deal_money_business-2-256.png",
+  "games":
+      "https://cdn2.iconfinder.com/data/icons/unigrid-phantom-devices-vol-4/60/017_184_sony_playstation_psp_video_console_games_gaming_device-256.png",
+  "shopping":
+      "https://cdn1.iconfinder.com/data/icons/shopping-346/24/buy-bag-cart-shop-shopping-512.png",
+  "others":
+      "https://cdn1.iconfinder.com/data/icons/smoothline-action/30/action_028-detail-more-info-others-256.png"
+};
