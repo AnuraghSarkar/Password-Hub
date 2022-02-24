ea64fa8c8de11a3efd803b115877b593ca45868e
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-24 21:57:22 +0545

    :clown_face: user service added

---
+++ client/lib/services/user_services/user_service.dart
@@ -0,0 +1,115 @@
+import 'package:get/get.dart';
+import 'package:client/main.dart';
+
+class UserService extends GetConnect {
+  //Get user details
+  Future<Response> getUser() {
+    var id = box.read("id");
+    var token = box.read("auth-token");
+
+    return get(
+      "http://192.168.1.72:3000/api/user/get-user/$id",
+      headers: {"auth-token": token},
+    );
+  }
+
+  //To update the Master password
+  Future<Response> updateMasterPassword(masterPass, newMasterPass) {
+    var id = box.read("id");
+    var token = box.read("auth-token");
+    var data = {
+      "masterPassword": masterPass,
+      "newMasterPassword": newMasterPass,
+    };
+
+    return put(
+      "http://192.168.1.72:3000/api/user/change-master-pass/$id",
+      data,
+      headers: {"auth-token": token},
+    );
+  }
+
+  //To update the password
+  Future<Response> updatePassword(password, newPassword) {
+    var id = box.read("id");
+    var token = box.read("auth-token");
+    var data = {
+      "password": password,
+      "newPassword": newPassword,
+    };
+
+    return put(
+      "http://192.168.1.72:3000/api/user/change-pass/$id",
+      data,
+      headers: {"auth-token": token},
+    );
+  }
+
+  //Updating user profile
+  Future<Response> updateUser(image, name) {
+    var id = box.read("id");
+    var token = box.read("auth-token");
+    //If only name is changed
+    if (image == "" && name != "") {
+      var data = {"name": name};
+      return put(
+        'http://192.168.1.72:3000/api/user/update/name/$id',
+        data,
+        headers: {"auth-token": token},
+      );
+    } else if (image != "" && name == "") {
+      //if only avatar is changed
+      var contentType = image.split('/').last.split('.').last;
+      final form = FormData({
+        'avatar': MultipartFile(
+          image,
+          filename: image.split("/").last,
+          contentType: 'image/$contentType',
+        ),
+      });
+      return put(
+        'http://192.168.1.72:3000/api/user/update/avatar/$id',
+        form,
+        headers: {"auth-token": token},
+      );
+    } else {
+      //If both avatar and name is changed
+      var contentType = image.split('/').last.split('.').last;
+      final form = FormData({
+        'avatar': MultipartFile(
+          image,
+          filename: image.split("/").last,
+          contentType: 'image/$contentType',
+        ),
+        'name': name.toString(),
+      });
+      return put(
+        'http://192.168.1.72:3000/api/user/update/avatar/$id',
+        form,
+        headers: {"auth-token": token},
+      );
+    }
+  }
+
+  //Removing profile picture
+  Future<Response> removeAvatar() {
+    var id = box.read("id");
+    var token = box.read("auth-token");
+    var data = {"avatar": ""};
+    return put(
+      "http://192.168.1.72:3000/api/user/remove-avatar/$id",
+      data,
+      headers: {"auth-token": token},
+    );
+  }
+
+  //To delete account
+  Future<Response> deleteAccount() {
+    var id = box.read("id");
+    var token = box.read("auth-token");
+    return delete(
+      "http://192.168.1.72:3000/api/user/delete-user/$id",
+      headers: {"auth-token": token},
+    );
+  }
+}
---
+++ client/lib/themes/themes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Themes {
+  final lightTheme = ThemeData.light().copyWith(
+    primaryColor: Colors.tealAccent,
+    scaffoldBackgroundColor: Colors.white,
+    brightness: Brightness.light,
+    appBarTheme: AppBarTheme(
+      color: Colors.white,
+      iconTheme: IconThemeData(color: Colors.black),
+      textTheme: TextTheme(
+        headline6: GoogleFonts.poppins(
+            color: Colors.black, fontSize: 20, fontWeight: FontWeight.w600),
+      ),
+      titleTextStyle: GoogleFonts.poppins(color: Colors.black),
+      // elevation: 0,
+    ),
+  );
+  final darkTheme = ThemeData.dark().copyWith(
+    primaryColor: Colors.tealAccent,
+    scaffoldBackgroundColor: Colors.grey[900],
+    appBarTheme: AppBarTheme(
+      color: Colors.grey[900],
+      iconTheme: IconThemeData(color: Colors.white),
+      elevation: 0,
+    ),
+  );
+}
