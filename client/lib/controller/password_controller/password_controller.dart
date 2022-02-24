88f4e714c32f9e07552b0b79f64236f326d0c677
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-24 21:53:20 +0545

    :fire: controllers for password added

---
+++ client/lib/controller/password_controller/password_controller.dart
@@ -0,0 +1,82 @@
+import 'package:get/get.dart';
+import 'package:client/models/password_model.dart';
+import 'package:client/services/network_services/network_service.dart';
+import 'package:client/services/password_service/password_service.dart';
+import 'package:client/views/error_screens/server_down.dart';
+import 'package:client/widgets/user_widgets/unauthorized_widget.dart';
+
+import '../../main.dart';
+
+class PasswordController extends GetxController {
+  var passwordData = Map<String, Password>().obs;
+  var isLogedIn = false.obs;
+  var isLoading = true.obs;
+  var isAutherized = false.obs;
+  var isPasswordVisible = false.obs;
+  var isButtonEnabled = true.obs;
+  var deleteButtonText = "Delete".obs;
+  var isImportantButtonEnabled = true.obs;
+
+  late bool isConnected1;
+
+  @override
+  void onInit() {
+    checkLogged();
+    fetchPassword();
+    super.onInit();
+  }
+
+  checkLogged() {
+    var token = box.read("auth-token");
+
+    if (token == null) {
+      isLogedIn(false);
+    } else if (token == "") {
+      isLogedIn(false);
+    } else {
+      isLogedIn(true);
+    }
+  }
+
+  fetchPassword() async {
+    isConnected1 = await isConnected();
+
+    if (isConnected1) {
+      isLoading(true);
+      try {
+        var response = await PasswordService().getMainPassword();
+        if (response.statusCode == 200) {
+          isLoading(false);
+          var data = passwordFromJson(response.bodyString.toString());
+          passwordData({"Passwords": data});
+        } else if (response.statusCode == 401) {
+          isLoading(false);
+          Get.off(Unauthorized());
+        } else {
+          isLoading(false);
+          Get.to(ServerDown());
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+
+  togglePassword() {
+    (isPasswordVisible.value)
+        ? isPasswordVisible(false)
+        : isPasswordVisible(true);
+  }
+
+  toggleButton() {
+    (isButtonEnabled.value) ? isButtonEnabled(false) : isButtonEnabled(true);
+  }
+
+  changeDeleteText(text) {
+    deleteButtonText(text);
+  }
+
+  toggleImportantButtonEnable(bool value) {
+    isImportantButtonEnabled(value);
+  }
+}
